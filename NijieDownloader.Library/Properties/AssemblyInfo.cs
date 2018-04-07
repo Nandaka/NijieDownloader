b5122a22fe6eea2b34c2d49686b846571b26d340
Nandaka <nchek2000@gmail.com>
2018-04-07 05:05:41 -0700

    update version

--- NijieDownloader.Library/Properties/AssemblyInfo.cs
+++ NijieDownloader.Library/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NijieDownloader.Library")]
-[assembly: AssemblyCopyright("Copyright © Nandaka 2014-2017")]
+[assembly: AssemblyCopyright("Copyright © Nandaka 2014-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.1")]
-[assembly: AssemblyFileVersion("1.0.9.1")]
+[assembly: AssemblyVersion("1.0.9.2")]
+[assembly: AssemblyFileVersion("1.0.9.2")]
--- NijieDownloader.UI/Properties/AssemblyInfo.cs
+++ NijieDownloader.UI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nandaka")]
 [assembly: AssemblyProduct("NijieDownloader.UI")]
-[assembly: AssemblyCopyright("Copyright © Nandaka 2014-2017")]
+[assembly: AssemblyCopyright("Copyright © Nandaka 2014-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.9.1")]
-[assembly: AssemblyFileVersion("1.0.9.1")]
+[assembly: AssemblyVersion("1.0.9.2")]
+[assembly: AssemblyFileVersion("1.0.9.2")]
